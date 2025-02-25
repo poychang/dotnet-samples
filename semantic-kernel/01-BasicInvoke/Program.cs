ec41ea43ced1e9f7a6186a5ad5a74d0b7d48361b
Poy Chang <poypost@gmail.com>
2025-02-25 22:58:19 +0800

    add BasicInvoke sample project with Azure OpenAI integration

---
+++ semantic-kernel/01-BasicInvoke/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+var endpoint = config["AzureOpenAI:Endpoint"] ?? string.Empty;
+var apiKey = config["AzureOpenAI:Key"] ?? string.Empty;
+var deploymentName = "gpt-4o-mini";
+
+// Create a kernel with OpenAI chat completion
+var kernelBuilder = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(deploymentName, endpoint, apiKey);
+var kernel = kernelBuilder.Build();
+
+// Invoke the kernel with a prompt and display the result
+Console.WriteLine("# Response ------------------------------");
+var response = await kernel.InvokePromptAsync("What color is the sky?");
+Console.WriteLine(response.ToString());
+Console.WriteLine();
+
+// Invoke the kernel with a prompt and stream the results to the display
+Console.WriteLine("# Streaming ------------------------------");
+await foreach (var update in kernel.InvokePromptStreamingAsync("What color is the sky?"))
+{
+    Console.Write(update.ToString());
+}
